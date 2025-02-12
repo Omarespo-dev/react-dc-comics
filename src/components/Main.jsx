7662bc7e1ebf08f1e40019cdbccf901b26be73a9
Omarespo-dev <omaresposito21@gmail.com>
2025-02-12 18:55:34 +0100

    Added bonus

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,6 @@
+// Importo Banner
+import Banner from "./Banner/Banner"
+
 // Esporto gia la funzione Main
 export default function Main() {
     return (
@@ -8,42 +11,8 @@ export default function Main() {
                 </div>
             </div>
 
-            <div className="container-icon">
-                <div className="img-content">
-
-                    <section className="set-card">
-                        {/* img */}
-                        <img src="/img/buy-comics-digital-comics.png" alt="" />
-                        <span>DIGITAL COMICS</span>
-                    </section>
-
-
-                    <section className="set-card">
-                        {/* img */}
-                        <img src="/img/buy-comics-merchandise.png" alt="" />
-                        <span>DC MERCHANDISE</span>
-                    </section>
-
-                    <section className="set-card">
-                        {/* img */}
-                        <img src="/img/buy-comics-subscriptions.png" alt="" />
-                        <span>SUBSCRIPTION</span>
-                    </section>
-
-                    <section className="set-card">
-                        {/* img */}
-                        <img src="/img/buy-comics-shop-locator.png" alt="" />
-                        <span>COMIC SHOP LOCATOR</span>
-                    </section>
-
-                    <section className="set-card">
-                        {/* img */}
-                        <img src="/img/buy-dc-power-visa.svg" alt=""  id="set-svg"/>
-                        <span>DC POWER VISA</span>
-                    </section>
-                    
-                </div>
-            </div>
+            <Banner />
+            
         </main>
     )
 }
