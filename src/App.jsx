72dc346f8edaba90dc9f3c8521341c9f27a3d82f
Omarespo-dev <omaresposito21@gmail.com>
2025-02-13 17:27:00 +0100

    Added condition for <a> "active"

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ function App() {
   // ARRAY DEI LINK
   const linkHeader = [
     { id: 1, text: "CHARACTERS", url: "#", current: false },
-    { id: 2, text: "COMICS", url: "#", current: false },
-    { id: 3, text: "MOVIES", url: "#", current: true },
+    { id: 2, text: "COMICS", url: "#", current: true },
+    { id: 3, text: "MOVIES", url: "#", current: false },
     { id: 4, text: "TV", url: "#", current: false },
     { id: 5, text: "GAMES", url: "#", current: false },
-    { id: 6, text: "COLLECTIBLES", url: "#", current: true },
+    { id: 6, text: "COLLECTIBLES", url: "#", current: false },
     { id: 7, text: "VIDEOS", url: "#", current: false },
-    { id: 8, text: "FANS", url: "#", current: true },
+    { id: 8, text: "FANS", url: "#", current: false },
     { id: 9, text: "NEWS", url: "#", current: false },
-    { id: 10, text: "SHOP", url: "#", current: true }
+    { id: 10, text: "SHOP", url: "#", current: false }
   ];
 
   return (
--- src/components/otherComponents/NavHeader.jsx
+++ src/components/otherComponents/NavHeader.jsx
@@ -7,7 +7,7 @@ export default function NavHeader(props) {
         {/* Ora per mettere tutto il contenuto dell array di oggetti si usa il map*/}
         <div className="link">
             {props.linkSecond.map((link) => (
-                <a key={link.id} href={link.url}><span>{link.text}</span></a>
+                <a key={link.id} href={link.url} className={link.current ? 'active' : ''}><span>{link.text}</span></a>
             ))}
         </div>
 
